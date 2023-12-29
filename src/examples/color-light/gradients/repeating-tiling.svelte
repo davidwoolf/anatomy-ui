572a9cfa535758f32c2aa9c8ab0c1857c1bbc325
David Woolf <woolf.david@gmail.com>
2023-12-29 15:12:38 -0800

    repeating gradient examples

---
+++ src/examples/color-light/gradients/repeating-tiling.svelte
@@ -0,0 +1,63 @@
+<script>
+  import Example from "@components/example.svelte";
+  import CSSEditor from "@components/css-editor.svelte";
+
+  let code = [
+    {
+      selector: ".gradient",
+      value: [
+        {
+          property: "--first-stop",
+          value: "#9747ff 0 20%",
+          type: "text",
+        },
+        {
+          property: "--second-stop",
+          value: "transparent 20% 100%",
+          type: "text",
+        },
+        {
+          property: "background-image",
+          value: "radial-gradient(circle, var(--first-stop), var(--second-stop))",
+          type: "static",
+        },
+        {
+          property: "background-size",
+          value: "2rem 2rem",
+          type: "static",
+        },
+        {
+          property: "height",
+          value: "12rem",
+          type: "static",
+        },
+        {
+          property: "width",
+          value: "12rem",
+          type: "static",
+        },
+      ],
+    },
+  ];
+</script>
+
+<Example>
+  <svelte:fragment slot="preview">
+    <div
+      class="repeating-radial-gradient"
+      style:--first-stop={code[0].value[0].value}
+      style:--second-stop={code[0].value[1].value} />
+  </svelte:fragment>
+  <svelte:fragment slot="controls">
+    <CSSEditor value={code} on:update={({ detail }) => (code = detail.text)} />
+  </svelte:fragment>
+</Example>
+
+<style>
+  .repeating-radial-gradient {
+    background: radial-gradient(circle, var(--first-stop), var(--second-stop));
+    background-size: 1rem 1rem;
+    height: 12rem;
+    width: 12rem;
+  }
+</style>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-  import NextSection from "@components/next-section.svelte";
 </script>
 
 <section>
@@ -8,7 +7,20 @@
   <p>Gain deeper knowledge to making interfaces for the web</p>
 
   <div>
-    <NextSection href="/foundations">Foundations</NextSection>
+    <a href="/foundations">
+      <span>Foundations</span>
+      <svg
+        aria-hidden
+        width="20"
+        height="17"
+        viewBox="0 0 20 17"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M19.0415 8.5C19.0415 8.75635 18.939 8.99219 18.7339 9.18701L11.915 15.9854C11.71 16.1802 11.4946 16.2725 11.2485 16.2725C10.7461 16.2725 10.3564 15.9033 10.3564 15.3906C10.3564 15.1445 10.4385 14.8984 10.6025 14.7446L12.8994 12.4067L16.355 9.25879L13.8735 9.4126H0.902344C0.369141 9.4126 0 9.0332 0 8.5C0 7.9668 0.369141 7.5874 0.902344 7.5874H13.8735L16.3447 7.74121L12.8994 4.59326L10.6025 2.25537C10.4385 2.09131 10.3564 1.85547 10.3564 1.60938C10.3564 1.09668 10.7461 0.727539 11.2485 0.727539C11.4946 0.727539 11.71 0.80957 11.9355 1.03516L18.7339 7.81299C18.939 8.00781 19.0415 8.24365 19.0415 8.5Z"
+          fill="currentcolor" />
+      </svg>
+    </a>
   </div>
 </section>
 
@@ -63,6 +75,45 @@
     opacity: 0;
   }
 
+  a {
+    animation-name: pulse;
+    animation-duration: 10s;
+    animation-fill-mode: forwards;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease;
+    align-items: baseline;
+    border: 2px solid currentcolor;
+    color: var(--color-purple-400);
+    border-radius: 9999px;
+    display: flex;
+    font-size: var(--font-size-base);
+    font-weight: 500;
+    gap: 1rem;
+    width: max-content;
+    padding: 0.75rem 1.5rem;
+  }
+
+  a:hover {
+    background-color: currentColor;
+  }
+
+  svg {
+    position: relative;
+    inset-block-start: 1px;
+    transition: all 75ms ease;
+  }
+
+  a:hover span,
+  a:hover svg {
+    color: white;
+  }
+
+  @media (min-width: 768px) {
+    a {
+      font-size: var(--font-size-xl);
+    }
+  }
+
   @media (min-width: 788px) {
     h1 {
       font-size: var(--font-size-6xl);
@@ -92,6 +143,28 @@
 
     to {
       opacity: 1;
+    }
+  }
+
+  @keyframes pulse {
+    0% {
+      color: var(--color-purple-400);
+    }
+
+    25% {
+      color: var(--color-green-400);
+    }
+
+    50% {
+      color: var(--color-yellow-400);
+    }
+
+    75% {
+      color: var(--color-blue-400);
+    }
+
+    100% {
+      color: var(--color-purple-400);
     }
   }
 </style>
